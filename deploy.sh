9bace5fb41b66c6a9dbd53d042f08ff1645c5815
Titouan BION <titouan.bion@gmail.com>
2020-04-25 00:35:47 +0200

    feat: adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -o close-the-wardrobe-please .
+scp close-the-wardrobe-please dorne:/opt/sensors/bin/close-the-wardrobe-please
+scp close-the-wardrobe-please.service dorne:/tmp/close-the-wardrobe-please.service
+ssh dorne 'sudo cp /tmp/close-the-wardrobe-please.service /etc/systemd/system/close-the-wardrobe-please.service'
+
+ssh dorne 'sudo systemctl daemon-reload'
+ssh dorne 'sudo systemctl enable close-the-wardrobe-please.service'
+ssh dorne 'sudo systemctl start close-the-wardrobe-please.service'
