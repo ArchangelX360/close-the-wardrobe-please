8e4ba46a0b6921307f8e444d8534c1870ea5cae7
Titouan BION <titouan.bion@gmail.com>
2020-04-25 23:34:42 +0200

    fix: re-deploy was not working

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -o close-the-wardrobe-please .
+ssh dorne 'sudo systemctl stop close-the-wardrobe-please.service'
 scp close-the-wardrobe-please dorne:/opt/sensors/bin/close-the-wardrobe-please
 scp close-the-wardrobe-please.service dorne:/tmp/close-the-wardrobe-please.service
 ssh dorne 'sudo cp /tmp/close-the-wardrobe-please.service /etc/systemd/system/close-the-wardrobe-please.service'
